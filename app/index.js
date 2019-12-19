f6685cf3c7c7657945f617a35e28412cbab338b3
Wesley Augusto Isotton <54046750+wesauis@users.noreply.github.com>
2019-12-19 11:53:34 -0300

    canvas setup

--- app/index.js
+++ app/index.js
@@ -1,7 +1,20 @@
-import { COLORS } from "./config.js";
+import { COLORS, SIZE } from "./config.js";
 
 // set color variavles on css
 Object.keys(COLORS).forEach(key => {
   document.body.style.setProperty(`--${key}`, COLORS[key]);
 });
+
+// setup canvas
+const canvas = document.querySelector("canvas#game");
+const context = canvas.getContext("2d");
+
+// resize canvas
+const width = window.innerWidth - (window.innerWidth % SIZE);
+const height = window.innerHeight - (window.innerHeight % SIZE);
+
+canvas.style.width = `${width}px`;
+canvas.style.height = `${height}px`;
+canvas.width = width / SIZE;
+canvas.height = height / SIZE;
 
