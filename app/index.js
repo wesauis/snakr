49433d2efeae1112e020708ea97fc7285824e6fc
Wesley Augusto Isotton <54046750+wesauis@users.noreply.github.com>
2019-12-23 13:59:19 -0300

    handle game-end and score changes

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import {
   COLORS,
   DEFAULT_SIZE
 } from "./config.js";
-import Snake from "./snake.js";
+import Game from "./game.js"
 
 // set color variavles on css
 Object.keys(COLORS).forEach(key => {
@@ -34,83 +34,21 @@ window.requestAnimationFrame(function render() {
   window.requestAnimationFrame(render)
 });
 
-function createFruit() {
-  return {
-    x: Math.floor(Math.random() * canvas.width),
-    y: Math.floor(Math.random() * canvas.height)
-  };
-}
-let fruit = undefined;
-
-// score spans
-const $hiScore = document.querySelector("#hi span");
-$hiScore.innerText = localStorage.getItem("hi_score") || 0;
-const $score = document.querySelector("#score span");
-
-// drawloop
-window.requestAnimationFrame(function draw() {
-  if (!fruit) fruit = createFruit();
-
-  // draw grid
-  grid.forEach(point => {
-    if (!((point.x % 2 === 0) === (point.y % 2 === 0)))
-      context.fillStyle = COLORS["mono-darker"];
-    else context.fillStyle = COLORS["mono-dark"];
-    context.fillRect(point.x, point.y, 1, 1);
-  });
-
-  // draw fruit
-  context.fillStyle = COLORS["primary-light"];
-  context.fillRect(fruit.x, fruit.y, 1, 1);
-
-  // draw snake
-  context.fillStyle = COLORS["mono-light"];
-  snake.cells.forEach((cell, index) => {
-    // head
-    if (index === snake.cells.length - 1)
-      context.fillStyle = COLORS["primary-dark"];
-
-    //tail
-    context.fillRect(cell.x, cell.y, 1, 1);
-  });
-
-  window.requestAnimationFrame(draw);
-});
-
-let updateID;
-window.addEventListener("keydown", event => {
-  const DIRCODES = {
-    ArrowUp: "Up",
-    ArrowDown: "Down",
-    ArrowLeft: "Left",
-    ArrowRight: "Right"
-  }
-  if (!snake.setDirection(DIRCODES[event.code])) return;
-  snake.update();
-  if (updateID) clearInterval(updateID);
-  updateID = setInterval(snake.update, 1000 / 5);
-});
-
-snake.addEventListener("death", () => {
-  const max = Math.max(+$hiScore.innerText, +$score.innerText)
-  localStorage.setItem("hi_score", max);
+game.addEventListener("end-game", event => {
   location.reload();
-});
+})
 
-snake.addEventListener("head_position", ({
-  data
-}) => {
-  if (data.x === fruit.x && data.y === fruit.y) {
-    fruit = undefined;
-    snake.grow();
+const $score = document.querySelector("#score span");
+const $hiScore = document.querySelector("#hi span");
 
-    $score.innerText = +$score.innerText + 1;
-  }
-});
+let hiScore = localStorage.getItem("high-score") || 0;
 
-snake.addEventListener("head_position", () => {
-  if (snake.cells.length > grid.length * 0.92) {
-    alert("YOU WIN!");
-    location.reload();
-  }
-});
+function updateScore(score) {
+  $score.innerText = score;
+
+  hiScore = Math.max(hiScore, score);
+  $hiScore.innerText = hiScore;
+  localStorage.setItem("high-score", hiScore);
+}
+updateScore(0);
+game.addEventListener("score", event => updateScore(event.data));
